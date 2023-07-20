bcbebff14257126ae458148722d2ffbbdae232e6
TetiPalii <TetianaPalii9491@gmail.com>
2023-07-20 19:45:39 +0200

    persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,12 +13,13 @@ import {
 
 export const store = configureStore({
   reducer: { contacts: contactsReducer, filter: filterReducer },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-    }),
+    });
+  },
 });
 
 export const persistor = persistStore(store);
